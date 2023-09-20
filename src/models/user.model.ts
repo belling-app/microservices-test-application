6490ce708542b46170a9479b9f40550ce2e73e5d
Agustín Lanusse <lanusseagustin@gmail.com>
2023-09-20 02:57:39 +0200

    modified user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -15,4 +15,13 @@ const userSchema = new Schema({
   }
 })
 
+userSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    ret.id = doc._id
+    delete ret.password
+    delete ret._id
+    delete ret.__v
+  }
+})
+
 export default model('User', userSchema)
