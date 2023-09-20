1b54de3c965f6849cd4060a477abc3dc49ea7e03
Agustín Lanusse <lanusseagustin@gmail.com>
2023-09-20 19:55:01 +0200

    change mongodb objectid to uuid

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,6 +1,11 @@
 import { model, Schema } from 'mongoose'
+import { v4 as uuidv4 } from 'uuid'
 
 const userSchema = new Schema({
+  _id: {
+    type: String,
+    default: uuidv4
+  },
   username: {
     type: String,
     required: true
